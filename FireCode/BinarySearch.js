204ba7c26d69f723dddb70811cfb9aa37c902669
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2021-11-14 21:32:43 +0600

    temporary code is added

---
+++ FireCode/BinarySearch.js
@@ -0,0 +1,10 @@
+function binarySearch(arr, n) {
+	let length = arr.length;
+	let position = length / 2;
+	while (arr[position] !== n) {
+		
+	}
+}
+
+const result = binarySearch([2,5,9],9);
+console.log(result);
