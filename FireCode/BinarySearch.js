04990fdaf67b02205551e3ace453f926c66933fc
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2021-12-30 18:45:24 +0600

    solved binary search with new implementtation

--- FireCode/BinarySearch.js
+++ FireCode/BinarySearch.js
@@ -1,3 +1,4 @@
+// my first solution
 function binarySearch(arr, n) {
 	let leftIndex = 0;
 	let rightIndex = arr.length - 1;
@@ -25,3 +26,22 @@ function binarySearch(arr, n) {
 
 const result = binarySearch([2,5,9],9);
 console.log(result);
+
+
+// binary search second solution without recursion
+function binarySearch(arr, n) {
+	let left = 0, right = arr.length - 1;
+	let mid = Math.floor((left + right) / 2);
+
+	while (left <= right) {
+		if (arr[mid] > n) {
+			right = mid - 1;
+		} else if (arr[mid] < n) {
+			left = mid + 1;
+		} else {
+			return true;
+		}
+		mid = Math.floor((left + right) / 2);
+	}
+	return false;
+}
