7bf1e03760f9f8f5b3907641d91ed313cb1e56e3
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2022-01-11 11:24:13 +0600

    solved  compress problem

--- FireCode/SimpleSortedStringCompression.js
+++ FireCode/SimpleSortedStringCompression.js
@@ -1,14 +1,35 @@
-const str = "AAAbbCCCD";
-const length  = str.length;
-let newStr = "", count = 1;
-for (let i = 0; i < length -1; i++) {
-	if (str[i] === str[i+1]) {
-		count++;
-		newStr += str[i] + count;
-		i++;
-	} else {
-		count = 0;
+function compress(input) {
+    const length  = input.length;
+	let newStr = "", count = 1;
+	for (let i = 0; i < length; i++) {
+		if (input[i] === input[i+1]) {
+			count++;
+		} else {
+			newStr += count === 1 ? input[i] : input[i] + count;
+			count = 1;
+		}
 	}
+	return newStr;
 }
 
-console.log(newStr);
+const result = compress("AAAAaaabbbbbcccc");
+console.log(result);
+
+
+// top solutions
+
+// compress(input) {
+//     let builder = "";
+//     let accumulator = 0;
+//     for (let i = 0; i < input.length; i++) {
+//       accumulator += 1;
+//       if (i + 1 === input.length || input[i] !== input[i + 1]) {
+//         builder += input[i];
+//         if (accumulator > 1) {
+//           builder += accumulator;
+//         }
+//         accumulator = 0;
+//       }
+//     }
+//     return builder;
+// }
