0f78ac79a771dbb6448fbf3b74890ff97143a253
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2022-06-06 22:01:20 +0600

    744. Find Smallest Letter Greater Than Target

--- LeetCode/744.FindSmallestLetterGreaterThanTarget.js
+++ LeetCode/744.FindSmallestLetterGreaterThanTarget.js
@@ -7,16 +7,14 @@
  function binarySearch(letters, target, left, right) {
 	const mid = Math.floor((left + right) / 2);
 
-	if (left >= right) {
-		return letters[mid];
+	if (letters[mid] <= target && letters[mid+1] > target) {
+		return letters[mid+1];
 	}
 
-	if (letters[mid] < target) {
-		return binarySearch(letters, target, left + 1, right);
+	if (letters[mid] <= target) {
+		return binarySearch(letters, target, mid + 1, right);
 	} else if (letters[mid] > target) {
-		return binarySearch(letters, target, left, right - 1);
-	} else {
-		return binarySearch(letters, target, left + 1, right);
+		return binarySearch(letters, target, left, mid - 1);
 	}
 }
 
