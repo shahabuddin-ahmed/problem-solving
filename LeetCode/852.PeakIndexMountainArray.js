29883f64b753725bbe8157757be6aac6e5f5d4f7
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2022-06-06 22:22:17 +0600

    852. Peak Index in a Mountain Array

--- LeetCode/852.PeakIndexMountainArray.js
+++ LeetCode/852.PeakIndexMountainArray.js
@@ -6,16 +6,14 @@
  function binarySearch(arr, left, right) {
 	const mid = Math.floor((left + right) / 2);
 
-	if (left >= right) {
-		return mid;
+	if (arr[mid] <= arr[mid+1] && arr[mid+1] > arr[mid+2]) {
+		return mid+1;
 	}
 
 	if (arr[mid] < arr[mid+1]) {
-		return binarySearch(arr, left + 1, right);
+		return binarySearch(arr, mid + 1, right);
 	} else if (arr[mid] > arr[mid+1]) {
-		return binarySearch(arr, left, right - 1);
-	} else {
-		return mid;
+		return binarySearch(arr, left, mid - 1);
 	}
 }
 
