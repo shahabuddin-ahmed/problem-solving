6216f7763f61f3c66b4cfe90e95d08b78b213a39
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2022-08-28 22:41:31 +0600

    Find the kth largest node of the binary search tree

---
+++ FireCode/FindKthLargestNodeBinarySearchTree.js
@@ -0,0 +1,18 @@
+const TreeNode = require("./TreeNode");
+
+class Solution {
+	/**
+	 * Returns the data of the kth largest node
+	 * in the passed binary search tree.
+	 *
+	 * @param {TreeNode} root Root TreeNode of the binary search tree.
+	 * @param {number} k Target (kth largest).
+	 * @return {number} Data of the Kth largest node.
+	 */
+	kthLargest(root, k) {
+		
+	}
+
+}
+
+module.exports = Solution;
