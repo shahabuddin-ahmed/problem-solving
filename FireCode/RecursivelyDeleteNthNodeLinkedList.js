eed99a7f5c948cd008a5e208aa1ad405bec343af
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2022-09-08 14:10:25 +0600

    updated old solved code

--- FireCode/RecursivelyDeleteNthNodeLinkedList.js
+++ FireCode/RecursivelyDeleteNthNodeLinkedList.js
@@ -1,13 +1,25 @@
-function recursivelyDeleteAtIndex(n) {
-	if (head === null) {
-		return null;
-	} else if (n === 0) {
-		return head.next;
-	}
+const ListNode = require("./ListNode");
 
-	head.next = this.deleteAtIndex(head.next, n - 1);
-	return head;
+class Solution {
+	/**
+	 * Recursively deletes the node at the nth index of the input linked
+	 * list. If n is out of bounds, returns the list without
+	 * modification.
+	 *
+	 * @param {ListNode} head Head of the singly linked list.
+	 * @param {number} n Index (zero-indexed) of the node to be deleted.
+	 * @return {ListNode} Head ListNode of the modified linked list.
+	 */
+	deleteAtIndex(head, n) {
+		if (head === null) {
+			return null;
+		} else if (n === 0) {
+			return head.next;
+		}
+
+		head.next = this.deleteAtIndex(head.next, n - 1);
+		return head;
+	}
 }
 
-const result = recursivelyDeleteAtIndex(3);
-console.log(result);
+module.exports = Solution;
