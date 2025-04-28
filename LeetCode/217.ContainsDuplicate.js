8582ff8cc770fa6c6b2f8c2ca7c25ba22171812a
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2025-04-28 15:58:04 +0600

    added new solutions for duplicate check

--- LeetCode/217.ContainsDuplicate.js
+++ LeetCode/217.ContainsDuplicate.js
@@ -13,3 +13,18 @@ function containsDuplicate(nums) {
 
 const result = containsDuplicate([1,1,1,3,3,4,3,2,4,2]);
 console.log(result);
+
+
+const containsDuplicate2 = (nums) => {
+	const set = new Set();
+	for (let i = 0; i < nums.length; i++) {
+		if (set.has(nums[i])) {
+			return true;
+		}
+		set.add(nums[i]);
+	}
+	return false;
+}
+
+const result2 = containsDuplicate2([1,1,1,3,3,4,3,2,4,2]);
+console.log(result2);
