5b43faed4fe8f0859b17c4ad54d25bcdb1c39944
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2025-05-13 17:45:44 +0600

    missing number another version

--- LeetCode/268.MissingNumber.js
+++ LeetCode/268.MissingNumber.js
@@ -6,3 +6,24 @@ function missingNumber(nums) {
 
 const result = missingNumber([9,6,4,2,3,5,7,0,1]);
 console.log(result);
+
+
+// missing number with -v
+var missingNumber2 = function(nums) {
+    let numSet = new Set(nums);
+    
+    let min = Math.min(...nums);
+    let max = Math.max(...nums);
+
+    for (let i = min; i <= max; i++) {
+        if (!numSet.has(i)) {
+            return i;
+        }
+    }
+    
+    // If no missing number in between, maybe next number
+    return max + 1;
+};
+
+const result2 = missingNumber2([9,6,4,2,3,5,7,0,1]);
+console.log(result2);
