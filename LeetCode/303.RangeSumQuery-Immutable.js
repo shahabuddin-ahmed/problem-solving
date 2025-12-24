aa40acf25c502149ceeda75dd6e491fd56f56e80
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2025-12-24 11:40:01 +0600

    Updated range summary

--- LeetCode/303.RangeSumQuery-Immutable.js
+++ LeetCode/303.RangeSumQuery-Immutable.js
@@ -1,6 +1,39 @@
 /**
  * @param {number[]} nums
  */
+var NumArray = function (nums) {
+    this.aggreSumArr = [0];
+    for (let i = 0; i < nums.length; i++) {
+        this.aggreSumArr[i + 1] = this.aggreSumArr[i] + nums[i];
+    }
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+    console.log(this.aggreSumArr);
+    return this.aggreSumArr[right + 1] - this.aggreSumArr[left];
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+
+const result = new NumArray([1,2,3,4,5,6,7]);
+console.log(result.sumRange(2, 3));
+
+
+
+
+
+/**
+ * @param {number[]} nums
+ */
  var NumArray = function(nums) {
 	this.nums = nums;
     // this.sum = nums.reduce((prev, curr) => prev + curr);
