5a5d814c154d190f80546be5aa21735f2a0a9aaf
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2026-02-27 13:12:41 +0600

    Updated plaindrom linkedlist

--- LeetCode/234.PalindromeLinkedList.js
+++ LeetCode/234.PalindromeLinkedList.js
@@ -9,7 +9,7 @@
  * @param {ListNode} head
  * @return {boolean}
  */
- var isPalindrome = function(head) {
+var isPalindrome = function (head) {
     const arr = [];
     let current = head;
 
@@ -20,10 +20,45 @@
 
     const len = arr.length;
 
-    for (let i = 0, j = len -1; i <= j; i++, j--) {
+    for (let i = 0, j = len - 1; i <= j; i++, j--) {
         if (arr[i] !== arr[j]) {
             return false;
         }
+    }
+
+    return true;
+};
+
+// Another solutions.
+
+var isPalindrome = function (head) {
+    if (!head || !head.next) {
+        return true;
+    }
+
+    let slow = head,
+        fast = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    let prev = null;
+    while (slow) {
+        let next = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = next;
+    }
+
+    let first = head,
+        second = prev;
+    while (second) {
+        if (first.val !== second.val) {
+            return false;
+        }
+        first = first.next;
+        second = second.next;
     }
 
     return true;
